0cee5a95f538c4ee3155bbbf5d36499f1cbab782
Yakima Teng <cleveryun@163.com>
2019-02-02 18:03:03 +0800

    :sparkles: 支持转发请求，本地开发时自动寻找寻找可用端口

--- build/tasks/serve.js
+++ build/tasks/serve.js
@@ -1,9 +1,27 @@
+const portfinder = require('portfinder')
+const proxy = require('http-proxy-middleware')
 const { browserSync, join } = require('../utils')
+const { proxyConfig } = require('../../config')
 
 module.exports = () => {
-  return browserSync.init({
-    server: {
-      baseDir: join('/dist')
-    }
-  })
+  portfinder.basePort = process.env.PORT || 8080
+  return portfinder.getPortPromise()
+    .then((port) => {
+      browserSync.init({
+        server: {
+          baseDir: [join('/dist')],
+          directory: true,
+          index: 'index.html',
+          routes: {
+            '/test': 'test'
+          }
+        },
+        port,
+        startPath: '/index.html',
+        middleware: Object.keys(proxyConfig).map((key) => proxy(key, proxyConfig[key]))
+      })
+    })
+    .catch((err) => {
+      console.log(`[Error]: ${err.message}`)
+    })
 }
--- config.js
+++ config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  /**
+   * 如果需要转发请求，可配置`proxy`项，否则可忽略
+   */
+  proxyConfig: {
+    '/path/api': {
+      target: 'http://111.22.333.4',
+      changeOrigin: true
+    }
+  },
+  /**
+   * renderData里的数据是用来渲染ejs模板文件用的
+   */
   renderData: {
     seo: {
       title: '页面标题',
